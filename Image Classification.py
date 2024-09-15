0c67b17c0db4de745c36797730fd81aa9d5c541b
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-09-15 16:55:24 -0400

    model change
    need data augmentation

--- Image Classification.py
+++ Image Classification.py
@@ -2,9 +2,10 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import models, layers
 from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras import layers, models, optimizers, losses
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 print(tf.__version__)
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
@@ -51,27 +52,31 @@ test_images, test_labels = next(iter(test_dataset))
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 model = models.Sequential()
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(256, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
+model.add(layers.Conv2D(32, (3,3), strides=(1,1), padding="same", activation='relu', input_shape=(256, 256, 3)))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(32, 3, activation='relu'))
+model.add(layers.MaxPool2D((2,2)))
 model.add(layers.Flatten())
-
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dense(256, activation='relu'))
+# model.add(layers.Dropout(0.5))
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(1, activation='softmax'))
+# model.add(layers.Dropout(0.5))
+model.add(layers.Dense(4, activation='softmax')) 
+print(model.summary())  # Prints the model summary
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# Use categorical crossentropy loss
+loss = losses.CategoricalCrossentropy(from_logits=False)
+optim = optimizers.Adam(learning_rate=0.0001)
+metrics = ['accuracy']
 
-print(model.summary()) #Prints the model summary
+model.compile(optimizer=optim, loss=loss, metrics=metrics)
+
+batch_size = 64
+epochs = 10
+
+# Convert labels to categorical
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=4)
+test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=4)
+
+model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, verbose=2)
+model.evaluate(test_images, test_labels, batch_size=batch_size, verbose=2)
