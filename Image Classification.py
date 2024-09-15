22edc23cc6eea22fa8455584a58ee8403f0598e3
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-09-14 23:27:20 -0400

    Added model, needs to be updated

--- Image Classification.py
+++ Image Classification.py
@@ -17,7 +17,14 @@ train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=32
 )
 
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    testing_data,
+    image_size=(256, 256),
+    batch_size=32
+)
+
 train_images, train_labels = next(iter(train_dataset))
+test_images, test_labels = next(iter(test_dataset))
 
 # Code for plotting
 def show(images, labels):
@@ -32,3 +39,16 @@ def show(images, labels):
     plt.show()
 
 show(train_images, train_labels)
+
+#Normalize
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = keras.models.Sequential()
+model.add(layers.Conv2D(32, (3,3), strides=(1,1), padding="valid", activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(32, 3, activation='relu'))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+print(model.summary())
