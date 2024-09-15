c13d66a4e50d8ba33752237b56bd63aa9725c27a
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-09-15 14:07:00 -0400

    Model implementation

--- Image Classification.py
+++ Image Classification.py
@@ -1,6 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import models, layers
+from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+print(tf.__version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,28 +34,44 @@ train_images, train_labels = next(iter(train_dataset))
 test_images, test_labels = next(iter(test_dataset))
 
 # Code for plotting
-def show(images, labels):
-    plt.figure(figsize=(10, 10))
-    for i in range(16):
-        plt.subplot(4, 4, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.xlabel(class_names[labels[i]])
-    plt.show()
+# def show(images, labels):
+#     plt.figure(figsize=(10, 10))
+#     for i in range(16):
+#         plt.subplot(4, 4, i + 1)
+#         plt.xticks([])
+#         plt.yticks([])
+#         plt.grid(False)
+#         plt.imshow(images[i].numpy().astype("uint8"))
+#         plt.xlabel(class_names[labels[i]])
+#     plt.show()
 
-show(train_images, train_labels)
+# show(train_images, train_labels)
 
 #Normalize
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-model = keras.models.Sequential()
-model.add(layers.Conv2D(32, (3,3), strides=(1,1), padding="valid", activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(32, 3, activation='relu'))
-model.add(layers.MaxPool2D((2,2)))
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
 model.add(layers.Flatten())
+
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-print(model.summary())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='softmax'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+print(model.summary()) #Prints the model summary
